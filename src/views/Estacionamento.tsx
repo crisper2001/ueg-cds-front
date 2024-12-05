5afd773d0807af0260ad989b3e75c40cfcf42ccb
Isaque <crisperohman@gmail.com>
2024-12-05 16:44:55 +0000

    ADD: Data e hora atuais em 'Estacionamento'

--- src/views/Estacionamento.tsx
+++ src/views/Estacionamento.tsx
@@ -4,6 +4,8 @@ import VagasAPI from "../utils/VagasAPI";
 export default function Estacionamento() {
 	const [vagas, setVagas] = useState<any[]>([]);
 	const [matrix, setMatrix] = useState<any[][]>([]);
+	const [currentDate, setCurrentDate] = useState<string>(new Date().toLocaleDateString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric' }));
+	const [currentTime, setCurrentTime] = useState<string>(new Date().toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' }));
 
 	const fetchVagas = async () => {
 		try {
@@ -31,10 +33,21 @@ export default function Estacionamento() {
 
 	useEffect(() => {
 		fetchVagas();
+		const timer = setInterval(() => {
+			const now = new Date();
+			setCurrentTime(now.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' }));
+			setCurrentDate(now.toLocaleDateString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric' }));
+		}, 1000);
+
+		return () => clearInterval(timer);
 	}, []);
 
 	return (
-		<div className="flex flex-col items-center justify-center">
+		<div className="flex flex-col items-center justify-center gap-4">
+			<div className="text-center">
+        <p className="text-2xl">Bem-vindo!</p>
+        <p className="text-lg">Hoje é {currentDate}. São {currentTime}.</p>
+			</div>
 			{vagas.length > 0 ? (
 				<>
 					<div className="matrix bg-base-300 p-2 rounded-lg">
@@ -49,7 +62,7 @@ export default function Estacionamento() {
 						))}
 					</div>
 
-					<div className="text-lg text-center">
+					<div className="text-lg text-center bg-base-300 p-2 rounded-lg">
 						Total de vagas: {vagas.length}
 					</div>
 				</>
@@ -61,3 +74,4 @@ export default function Estacionamento() {
 		</div>
 	);
 }
+
