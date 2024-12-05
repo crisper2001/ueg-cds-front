19acbc6313f4375017342fb8db9364c312e11bac
Isaque <crisperohman@gmail.com>
2024-12-05 16:12:29 +0000

    MOD: Cadastro de preços e vagas agora funciona de verdade

--- src/views/Precos.tsx
+++ src/views/Precos.tsx
@@ -3,6 +3,10 @@ import PricesAPI from "../utils/PricesAPI";
 
 export default function Precos() {
 	const [precos, setPrecos] = useState<any[]>([]);
+	const [tempoInicial, setTempoInicial] = useState<number>();
+	const [tempoAdicional, setTempoAdicional] = useState<number>();
+	const [valorInicial, setValorInicial] = useState<number>();
+	const [valorAdicional, setValorAdicional] = useState<number>();
 
 	const fetchPrecos = async () => {
 		try {
@@ -19,13 +23,18 @@ export default function Precos() {
 		fetchPrecos();
 	}, []);
 
-	const handleAddPreco = async () => {
+	const handleAddPreco = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+    if (tempoInicial === undefined || tempoAdicional === undefined || valorInicial === undefined || valorAdicional === undefined) {
+      alert("Preencha todos os campos");
+      return;
+    }
     try {
       const precoData = {
-        tempoInicial: 0,
-        tempoAdicional: 0,
-        valorInicial: 0,
-        valorAdicional: 0,
+        tempoInicial,
+        tempoAdicional,
+        valorInicial,
+        valorAdicional,
       };
       const response = await PricesAPI.createPrice(precoData);
       if (response) {
@@ -39,34 +48,94 @@ export default function Precos() {
 
 	return (
 		<div className="flex flex-col gap-4">
-			<table className="table w-full border-2 border-base-200">
-				<thead>
-					<tr className="bg-base-200">
-						<th>ID</th>
-						<th>Tempo Inicial (min)</th>
-						<th>Tempo Adicional (min)</th>
-						<th>Valor Inicial (R$)</th>
-						<th>Valor Adicional (R$)</th>
-					</tr>
-				</thead>
-				<tbody>
-					{precos.map((preco) => (
-						<tr key={preco.id}>
-							<td>{preco.id}</td>
-							<td>{preco.tempoInicial}</td>
-							<td>{preco.tempoAdicional}</td>
-							<td>{preco.valorInicial}</td>
-							<td>{preco.valorAdicional}</td>
+			{precos.length > 0 ? (
+				<table className="table w-full border-2 border-base-200">
+					<thead>
+						<tr className="bg-base-200">
+							<th>ID</th>
+							<th>Tempo Inicial (min)</th>
+							<th>Tempo Adicional (min)</th>
+							<th>Valor Inicial (R$)</th>
+							<th>Valor Adicional (R$)</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{precos.map((preco) => (
+							<tr key={preco.id}>
+								<td>{preco.id}</td>
+								<td>{preco.tempoInicial}</td>
+								<td>{preco.tempoAdicional}</td>
+								<td>{preco.valorInicial}</td>
+								<td>{preco.valorAdicional}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			) : (
+				<p className="text-lg text-center">Nenhum preço encontrado.</p>
+			)}
 
 			<div className="flex justify-end">
-				<button className="btn btn-primary text-white" onClick={handleAddPreco}>
+				<button className="btn btn-primary text-white" onClick={() => {
+					(document.getElementById('add_preco') as HTMLDialogElement)?.showModal();
+				}}>
 					+ Adicionar Preço
 				</button>
 			</div>
+
+      <dialog id="add_preco" className="modal">
+        <div className="modal-box w-96">
+          <h3 className="font-bold text-lg pb-4">Adicionar Preço</h3>
+          <form onSubmit={handleAddPreco} className="flex flex-col gap-4">
+            <div>
+              <input
+                type="number"
+                className="input input-bordered w-full"
+                placeholder="Tempo Inicial (min)"
+                value={tempoInicial}
+                onChange={(e) => setTempoInicial(Number(e.target.value))}
+              />
+            </div>
+            <div>
+              <input
+                type="number"
+                className="input input-bordered w-full"
+                placeholder="Tempo Adicional (min)"
+                value={tempoAdicional}
+                onChange={(e) => setTempoAdicional(Number(e.target.value))}
+              />
+            </div>
+            <div>
+              <input
+                type="number"
+                className="input input-bordered w-full"
+                placeholder="Valor Inicial (R$)"
+                value={valorInicial}
+                onChange={(e) => setValorInicial(Number(e.target.value))}
+              />
+            </div>
+            <div>
+              <input
+                type="number"
+                className="input input-bordered w-full"
+                placeholder="Valor Adicional (R$)"
+                value={valorAdicional}
+                onChange={(e) => setValorAdicional(Number(e.target.value))}
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary text-white w-1/3 mx-auto"
+            >
+              Adicionar
+            </button>
+          </form>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
 		</div>
 	);
 }
+
--- src/views/Vagas.tsx
+++ src/views/Vagas.tsx
@@ -2,54 +2,129 @@ import { useEffect, useState } from "react";
 import VagasAPI from "../utils/VagasAPI";
 
 export default function Vagas() {
-	const [vagas, setVagas] = useState<any[]>([]);
+  const [vagas, setVagas] = useState<any[]>([]);
+  const [numero, setNumero] = useState<number>();
+  const [locHorizontal, setLocHorizontal] = useState<number>();
+  const [locVertical, setLocVertical] = useState<number>();
 
-	const fetchVagas = async () => {
-		try {
-			const data = await VagasAPI.getAllVagas();
-			if (data) {
-				setVagas(data);
-			}
-		} catch (error) {
-			console.error('Error fetching all vagas:', error);
-		}
-	};
+  const fetchVagas = async () => {
+    try {
+      const data = await VagasAPI.getAllVagas();
+      if (data) {
+        setVagas(data);
+      }
+    } catch (error) {
+      console.error("Error fetching all vagas:", error);
+    }
+  };
 
-	useEffect(() => {
-		fetchVagas();
-	}, []);
+  useEffect(() => {
+    fetchVagas();
+  }, []);
 
-	return (
-		<div className="flex flex-col gap-4">
+  const handleAddVaga = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!numero || !locHorizontal || !locVertical) {
+      alert("Preencha todos os campos");
+      return;
+    }
+    try {
+      const vagaData = {
+        numero,
+        locHorizontal,
+        locVertical,
+      };
+      console.log(vagaData);
+      const response = await VagasAPI.createVaga(vagaData);
+      if (response) {
+        console.log("Vaga criada com sucesso:", response);
+        fetchVagas();
+      }
+    } catch (error) {
+      console.error("Error creating vaga:", error);
+    }
+  };
 
-			<table className="table w-full border-2 border-base-200">
-				<thead>
-					<tr className="bg-base-200">
-						<th>ID</th>
-						<th>Número</th>
-						<th>Localização Horizontal</th>
-						<th>Localização Vertical</th>
-					</tr>
-				</thead>
-				<tbody>
-					{vagas.map((vaga) => (
-						<tr key={vaga.id}>
-							<td>{vaga.id}</td>
-							<td>{vaga.numero}</td>
-							<td>{vaga.locHorizontal}</td>
-							<td>{vaga.locVertical}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+  return (
+    <div className="flex flex-col gap-4">
+      {vagas.length > 0 ? (
+        <table className="table w-full border-2 border-base-200">
+          <thead>
+            <tr className="bg-base-200">
+              <th>ID</th>
+              <th>Número</th>
+              <th>Localização Horizontal</th>
+              <th>Localização Vertical</th>
+            </tr>
+          </thead>
+          <tbody>
+            {vagas.map((vaga) => (
+              <tr key={vaga.id}>
+                <td>{vaga.id}</td>
+                <td>{vaga.numero}</td>
+                <td>{vaga.locHorizontal}</td>
+                <td>{vaga.locVertical}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-lg text-center">Nenhuma vaga encontrada.</p>
+      )}
 
-			<div className="flex justify-end">
-				<button className="btn btn-primary text-white">
-					+ Adicionar Vaga
-				</button>
-			</div>
+      <div className="flex justify-end">
+        <button
+          className="btn btn-primary text-white"
+          onClick={() => (document.getElementById("add_vaga") as HTMLDialogElement)?.showModal()}
+        >
+          + Adicionar Vaga
+        </button>
+      </div>
 
-		</div>
-	);
+      <dialog id="add_vaga" className="modal">
+        <div className="modal-box w-96">
+          <h3 className="font-bold text-lg pb-4">Adicionar Vaga</h3>
+          <form className="flex flex-col gap-4" onSubmit={handleAddVaga}>
+            <div>
+              <input
+                type="number"
+                className="input input-bordered w-full"
+                placeholder="Número"
+                value={numero}
+                onChange={(e) => setNumero(Number(e.target.value))}
+              />
+            </div>
+            <div>
+              <input
+                type="number"
+                className="input input-bordered w-full"
+                placeholder="Localização Horizontal"
+                value={locHorizontal}
+                onChange={(e) => setLocHorizontal(Number(e.target.value))}
+              />
+            </div>
+            <div>
+              <input
+                type="number"
+                className="input input-bordered w-full"
+                placeholder="Localização Vertical"
+                value={locVertical}
+                onChange={(e) => setLocVertical(Number(e.target.value))}
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary text-white w-1/3 mx-auto"
+            >
+              Adicionar
+            </button>
+          </form>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </div>
+  );
 }
 
